f54aaf7a7e3259ef56560d3d9bd83b5c28dc92f1
guillaume <guillaume.belanger27@gmail.com>
2022-06-08 11:55:48 -0400

    Bumps the lib version

--- lib/charms/tls_certificates_interface/v0/tls_certificates.py
+++ lib/charms/tls_certificates_interface/v0/tls_certificates.py
@@ -97,7 +97,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 18
+LIBPATCH = 19
 
 REQUIRER_JSON_SCHEMA = {
     "$schema": "http://json-schema.org/draft-04/schema#",
