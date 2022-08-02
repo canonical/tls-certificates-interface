5b0006dd28df382ad03640339746ba39621c50ef
guillaume <guillaume.belanger27@gmail.com>
2022-08-02 19:39:53 -0400

    Minor bug fixes and improves charm example

--- lib/charms/tls_certificates_interface/v1/tls_certificates.py
+++ lib/charms/tls_certificates_interface/v1/tls_certificates.py
@@ -29,7 +29,9 @@ parts:
 ```
 
 ### Provider charm
-The provider charm is the charm providing certificates to another charm that requires them.
+The provider charm is the charm providing certificates to another charm that requires them. In
+this example, the provider charm is storing its private key using a peer relation interface called
+`replicas`.
 
 Example:
 ```python
@@ -39,7 +41,7 @@ from charms.tls_certificates_interface.v1.tls_certificates import (
     generate_private_key,
 )
 from ops.charm import CharmBase, InstallEvent
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, WaitingStatus
 
 
 def generate_ca(private_key: bytes, subject: str) -> str:
@@ -64,19 +66,28 @@ class ExampleProviderCharm(CharmBase):
         private_key_password = b"banana"
         private_key = generate_private_key(password=private_key_password)
         ca_certificate = generate_ca(private_key=private_key, subject="whatever")
-        replicas = self.model.get_relation("replicas")
-        replicas.data[self.app].update(  # type: ignore[union-attr]
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        replicas_relation.data[self.app].update(
             {
                 "private_key_password": "banana",
                 "private_key": private_key,
                 "ca_certificate": ca_certificate,
             }
         )
+        self.unit.status = ActiveStatus()
 
     def _on_certificate_request(self, event: CertificateRequestEvent) -> None:
-        replicas = self.model.get_relation("replicas")
-        ca_certificate = replicas.data[self.app].get("ca_certificate")  # type: ignore[union-attr]
-        private_key = replicas.data[self.app].get("private_key")  # type: ignore[union-attr]
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        ca_certificate = replicas_relation.data[self.app].get("ca_certificate")
+        private_key = replicas_relation.data[self.app].get("private_key")
         certificate = generate_certificate(
             ca=ca_certificate,
             private_key=private_key,
@@ -90,7 +101,6 @@ class ExampleProviderCharm(CharmBase):
             chain=ca_certificate,
             relation_id=event.relation_id,
         )
-        self.unit.status = ActiveStatus()
 
 
 if __name__ == "__main__":
@@ -98,19 +108,21 @@ if __name__ == "__main__":
 ```
 
 ### Requirer charm
-The requirer charm is the charm requiring certificates from another charm that provides them.
+The requirer charm is the charm requiring certificates from another charm that provides them. In
+this example, the requirer charm is storing its certificates using a peer relation interface called
+`replicas`.
 
 Example:
 ```python
 from charms.tls_certificates_interface.v1.tls_certificates import (
-    CertificateExpiringEvent,
     CertificateAvailableEvent,
+    CertificateExpiringEvent,
     TLSCertificatesRequires,
     generate_csr,
     generate_private_key,
 )
 from ops.charm import CharmBase, RelationJoinedEvent
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, WaitingStatus
 
 
 class ExampleRequirerCharm(CharmBase):
@@ -129,54 +141,65 @@ class ExampleRequirerCharm(CharmBase):
     def _on_install(self, event) -> None:
         private_key_password = b"banana"
         private_key = generate_private_key(password=private_key_password)
-        replicas = self.model.get_relation("replicas")
-        replicas.data[self.app].update(  # type: ignore[union-attr]
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        replicas_relation.data[self.app].update(
             {"private_key_password": "banana", "private_key": private_key.decode()}
         )
 
     def _on_certificates_relation_joined(self, event: RelationJoinedEvent) -> None:
-        replicas = self.model.get_relation("replicas")
-        private_key_password = replicas.data[self.app].get("private_key_password")  # type: ignore[union-attr]  # noqa: E501, W505
-        private_key = replicas.data[self.app].get("private_key")  # type: ignore[union-attr]
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        private_key_password = replicas_relation.data[self.app].get("private_key_password")
+        private_key = replicas_relation.data[self.app].get("private_key")
         csr = generate_csr(
             private_key=private_key.encode(),
             private_key_password=private_key_password.encode(),
             subject="whatever",
         )
-        replicas.data[self.app].update({"csr": csr.decode()})  # type: ignore[union-attr]
+        replicas_relation.data[self.app].update({"csr": csr.decode()})
         self.certificates.request_certificate(csr=csr)
 
     def _on_certificate_available(self, event: CertificateAvailableEvent) -> None:
-        replicas = self.model.get_relation("replicas")
-        event_csr = event.certificate_signing_request
-        stored_csr = replicas.data[self.app].get("csr")  # type: ignore[union-attr]
-        if stored_csr == event_csr:
-            replicas.data[self.app].update({"certificate": event.certificate})  # type: ignore[union-attr]  # noqa: E501, W505
-            replicas.data[self.app].update({"ca": event.ca})  # type: ignore[union-attr]  # noqa: E501, W505
-            replicas.data[self.app].update({"chain": event.chain})  # type: ignore[union-attr]  # noqa: E501, W505
-            self.unit.status = ActiveStatus()
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        replicas_relation.data[self.app].update({"certificate": event.certificate})
+        replicas_relation.data[self.app].update({"ca": event.ca})
+        replicas_relation.data[self.app].update({"chain": event.chain})
+        self.unit.status = ActiveStatus()
 
     def _on_certificate_almost_expired(self, event: CertificateExpiringEvent) -> None:
-        replicas = self.model.get_relation("replicas")
-        private_key_password = replicas.data[self.app].get(  # type: ignore[union-attr]  # noqa: E501, W505
-            "private_key_password"
-        )
-        private_key = replicas.data[self.app].get("private_key")  # type: ignore[union-attr]
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        private_key_password = replicas_relation.data[self.app].get("private_key_password")
+        private_key = replicas_relation.data[self.app].get("private_key")
         new_csr = generate_csr(
             private_key=private_key.encode(),
             private_key_password=private_key_password.encode(),
             subject="whatever",
         )
         self.certificates.request_certificate(csr=new_csr)
-        existing_csr = replicas.data[self.app].get("csr")  # type: ignore[union-attr]
+        existing_csr = replicas_relation.data[self.app].get("csr")
         self.certificates.revoke_certificate(certificate_signing_request=existing_csr.encode())
-        replicas.data[self.app].update({"csr": new_csr.decode()})  # type: ignore[union-attr]
+        replicas_relation.data[self.app].update({"csr": new_csr.decode()})
 
 
 if __name__ == "__main__":
     main(ExampleRequirerCharm)
 ```
-"""
+"""  # noqa: D405, D410, D411, D214, D416
 
 import json
 import logging
@@ -304,7 +327,7 @@ class CertificateAvailableEvent(EventBase):
     def restore(self, snapshot: dict):
         """Restores snapshot."""
         self.certificate = snapshot["certificate"]
-        self.certificate_signing_request = snapshot["certificate"]
+        self.certificate_signing_request = snapshot["certificate_signing_request"]
         self.ca = snapshot["ca"]
         self.chain = snapshot["chain"]
 
@@ -608,7 +631,6 @@ class TLSCertificatesRequires(Object):
         """
         logger.info("Received request to create certificate")
         relation = self.model.get_relation(self.relationship_name)
-        print(relation)
         if not relation:
             message = (
                 f"Relation {self.relationship_name} does not exist - "
--- src/charm.py
+++ src/charm.py
@@ -14,7 +14,7 @@ from charms.tls_certificates_interface.v1.tls_certificates import (
     generate_private_key,
 )
 from ops.charm import CharmBase, InstallEvent, RelationJoinedEvent
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, WaitingStatus
 
 
 def generate_ca(private_key: bytes, subject: str) -> str:
@@ -43,19 +43,28 @@ class ExampleProviderCharm(CharmBase):
         private_key_password = b"banana"
         private_key = generate_private_key(password=private_key_password)
         ca_certificate = generate_ca(private_key=private_key, subject="whatever")
-        replicas = self.model.get_relation("replicas")
-        replicas.data[self.app].update(  # type: ignore[union-attr]
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        replicas_relation.data[self.app].update(
             {
                 "private_key_password": "banana",
                 "private_key": private_key,
                 "ca_certificate": ca_certificate,
             }
         )
+        self.unit.status = ActiveStatus()
 
     def _on_certificate_request(self, event: CertificateRequestEvent) -> None:
-        replicas = self.model.get_relation("replicas")
-        ca_certificate = replicas.data[self.app].get("ca_certificate")  # type: ignore[union-attr]
-        private_key = replicas.data[self.app].get("private_key")  # type: ignore[union-attr]
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        ca_certificate = replicas_relation.data[self.app].get("ca_certificate")
+        private_key = replicas_relation.data[self.app].get("private_key")
         certificate = generate_certificate(
             ca=ca_certificate,
             private_key=private_key,
@@ -69,7 +78,6 @@ class ExampleProviderCharm(CharmBase):
             chain=ca_certificate,
             relation_id=event.relation_id,
         )
-        self.unit.status = ActiveStatus()
 
 
 class ExampleRequirerCharm(CharmBase):
@@ -89,45 +97,56 @@ class ExampleRequirerCharm(CharmBase):
     def _on_install(self, event) -> None:
         private_key_password = b"banana"
         private_key = generate_private_key(password=private_key_password)
-        replicas = self.model.get_relation("replicas")
-        replicas.data[self.app].update(  # type: ignore[union-attr]
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        replicas_relation.data[self.app].update(
             {"private_key_password": "banana", "private_key": private_key.decode()}
         )
 
     def _on_certificates_relation_joined(self, event: RelationJoinedEvent) -> None:
-        replicas = self.model.get_relation("replicas")
-        private_key_password = replicas.data[self.app].get("private_key_password")  # type: ignore[union-attr]  # noqa: E501
-        private_key = replicas.data[self.app].get("private_key")  # type: ignore[union-attr]
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        private_key_password = replicas_relation.data[self.app].get("private_key_password")
+        private_key = replicas_relation.data[self.app].get("private_key")
         csr = generate_csr(
             private_key=private_key.encode(),
             private_key_password=private_key_password.encode(),
             subject="whatever",
         )
-        replicas.data[self.app].update({"csr": csr.decode()})  # type: ignore[union-attr]
+        replicas_relation.data[self.app].update({"csr": csr.decode()})
         self.certificates.request_certificate(csr=csr)
 
     def _on_certificate_available(self, event: CertificateAvailableEvent) -> None:
-        replicas = self.model.get_relation("replicas")
-        event_csr = event.certificate_signing_request
-        stored_csr = replicas.data[self.app].get("csr")  # type: ignore[union-attr]
-        if stored_csr == event_csr:
-            replicas.data[self.app].update({"certificate": event.certificate})  # type: ignore[union-attr]  # noqa: E501
-            replicas.data[self.app].update({"ca": event.ca})  # type: ignore[union-attr]  # noqa: E501
-            replicas.data[self.app].update({"chain": event.chain})  # type: ignore[union-attr]  # noqa: E501
-            self.unit.status = ActiveStatus()
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        replicas_relation.data[self.app].update({"certificate": event.certificate})
+        replicas_relation.data[self.app].update({"ca": event.ca})
+        replicas_relation.data[self.app].update({"chain": event.chain})
+        self.unit.status = ActiveStatus()
 
     def _on_certificate_almost_expired(self, event: CertificateExpiringEvent) -> None:
-        replicas = self.model.get_relation("replicas")
-        private_key_password = replicas.data[self.app].get(  # type: ignore[union-attr]  # noqa: E501
-            "private_key_password"
-        )
-        private_key = replicas.data[self.app].get("private_key")  # type: ignore[union-attr]
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        private_key_password = replicas_relation.data[self.app].get("private_key_password")
+        private_key = replicas_relation.data[self.app].get("private_key")
         new_csr = generate_csr(
             private_key=private_key.encode(),
             private_key_password=private_key_password.encode(),
             subject="whatever",
         )
         self.certificates.request_certificate(csr=new_csr)
-        existing_csr = replicas.data[self.app].get("csr")  # type: ignore[union-attr]
+        existing_csr = replicas_relation.data[self.app].get("csr")
         self.certificates.revoke_certificate(certificate_signing_request=existing_csr.encode())
-        replicas.data[self.app].update({"csr": new_csr.decode()})  # type: ignore[union-attr]
+        replicas_relation.data[self.app].update({"csr": new_csr.decode()})
