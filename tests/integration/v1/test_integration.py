f17abf709ed1c190566c00b907472a9974fc75ee
guillaume <guillaume.belanger27@gmail.com>
2022-09-14 17:04:16 -0400

    Adds integration tests for the case where there are multiple requirers

--- tests/integration/v1/test_integration.py
+++ tests/integration/v1/test_integration.py
@@ -5,9 +5,8 @@
 import logging
 import shutil
 
-import pytest
-
 logger = logging.getLogger(__name__)
+
 LIB_DIR = "lib/charms/tls_certificates_interface/v1/tls_certificates.py"
 REQUIRER_CHARM_DIR = "tests/integration/v1/requirer_charm"
 PROVIDER_CHARM_DIR = "tests/integration/v1/provider_charm"
@@ -21,20 +20,19 @@ def copy_lib_content() -> None:
 
 
 class TestIntegration:
-    @pytest.mark.abort_on_fail
-    async def build_and_deploy_requirer_charm(self, ops_test):
-        charm = await ops_test.build_charm(f"{REQUIRER_CHARM_DIR}/")
-        await ops_test.model.deploy(charm, application_name=TLS_CERTIFICATES_REQUIRER_APP_NAME)
-
-    @pytest.mark.abort_on_fail
-    async def build_and_deploy_provider_charm(self, ops_test):
-        charm = await ops_test.build_charm(f"{PROVIDER_CHARM_DIR}/")
-        await ops_test.model.deploy(charm, application_name=TLS_CERTIFICATES_PROVIDER_APP_NAME)
+    requirer_charm = None
+    provider_charm = None
 
     async def test_given_charms_packed_when_deploy_charm_then_status_is_blocked(self, ops_test):
         copy_lib_content()
-        await self.build_and_deploy_requirer_charm(ops_test)
-        await self.build_and_deploy_provider_charm(ops_test)
+        TestIntegration.requirer_charm = await ops_test.build_charm(f"{REQUIRER_CHARM_DIR}/")
+        TestIntegration.provider_charm = await ops_test.build_charm(f"{PROVIDER_CHARM_DIR}/")
+        await ops_test.model.deploy(
+            TestIntegration.requirer_charm, application_name=TLS_CERTIFICATES_REQUIRER_APP_NAME
+        )
+        await ops_test.model.deploy(
+            TestIntegration.provider_charm, application_name=TLS_CERTIFICATES_PROVIDER_APP_NAME
+        )
 
         await ops_test.model.wait_for_idle(
             apps=[TLS_CERTIFICATES_REQUIRER_APP_NAME, TLS_CERTIFICATES_PROVIDER_APP_NAME],
@@ -62,7 +60,38 @@ class TestIntegration:
         action_output = await ops_test.model.get_action_output(
             action_uuid=action.entity_id, wait=60
         )
+        assert action_output["return-code"] == 0
+        assert "ca" in action_output and action_output["ca"] is not None
+        assert "certificate" in action_output and action_output["certificate"] is not None
+        assert "chain" in action_output and action_output["chain"] is not None
 
+    async def test_given_additional_requirer_charm_deployed_when_relate_then_requirer_received_certs(  # noqa: E501
+        self, ops_test
+    ):
+        new_requirer_app_name = "new-tls-requirer"
+        await ops_test.model.deploy(
+            TestIntegration.requirer_charm, application_name=new_requirer_app_name
+        )
+        await ops_test.model.add_relation(
+            relation1=new_requirer_app_name,
+            relation2=TLS_CERTIFICATES_PROVIDER_APP_NAME,
+        )
+        await ops_test.model.wait_for_idle(
+            apps=[
+                TLS_CERTIFICATES_REQUIRER_APP_NAME,
+                TLS_CERTIFICATES_PROVIDER_APP_NAME,
+                new_requirer_app_name,
+            ],
+            status="active",
+            timeout=1000,
+        )
+        requirer_unit = ops_test.model.units[f"{new_requirer_app_name}/0"]
+
+        action = await requirer_unit.run_action(action_name="get-certificate")
+
+        action_output = await ops_test.model.get_action_output(
+            action_uuid=action.entity_id, wait=60
+        )
         assert action_output["return-code"] == 0
         assert "ca" in action_output and action_output["ca"] is not None
         assert "certificate" in action_output and action_output["certificate"] is not None
