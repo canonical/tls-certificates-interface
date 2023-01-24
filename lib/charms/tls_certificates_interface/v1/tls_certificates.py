de64adc133e0d9d5e47cf0dd3a6efe4ccbca2ced
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-01-24 09:58:54 +0100

    Bumps LIBPATCH for v1

--- lib/charms/tls_certificates_interface/v1/tls_certificates.py
+++ lib/charms/tls_certificates_interface/v1/tls_certificates.py
@@ -272,7 +272,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 10
+LIBPATCH = 11
 
 
 REQUIRER_JSON_SCHEMA = {
