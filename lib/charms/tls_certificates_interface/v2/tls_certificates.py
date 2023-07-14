c19b0a2e3737b2c5203f9bab09881752bf333ccc
Luca Bello <luca.bello@canonical.com>
2023-07-14 14:07:38 +0000

    fix missing leader guard

--- lib/charms/tls_certificates_interface/v2/tls_certificates.py
+++ lib/charms/tls_certificates_interface/v2/tls_certificates.py
@@ -309,7 +309,7 @@ LIBAPI = 2
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 8
+LIBPATCH = 9
 
 PYDEPS = ["cryptography", "jsonschema"]
 
@@ -1391,13 +1391,14 @@ class TLSCertificatesRequiresV2(Object):
                                 ),
                             )
                         except SecretNotFoundError:
-                            secret = self.charm.app.add_secret(
-                                {"certificate": certificate["certificate"]},
-                                label=f"{LIBID}-{certificate['certificate_signing_request']}",
-                                expire=self._get_next_secret_expiry_time(
-                                    certificate["certificate"]
-                                ),
-                            )
+                            if self.model.unit.is_leader():
+                                secret = self.charm.app.add_secret(
+                                    {"certificate": certificate["certificate"]},
+                                    label=f"{LIBID}-{certificate['certificate_signing_request']}",
+                                    expire=self._get_next_secret_expiry_time(
+                                        certificate["certificate"]
+                                    ),
+                                )
                     self.on.certificate_available.emit(
                         certificate_signing_request=certificate["certificate_signing_request"],
                         certificate=certificate["certificate"],
