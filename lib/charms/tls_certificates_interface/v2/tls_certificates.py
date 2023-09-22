48d05b6d8aa049e64f332933997ce831fc96abd6
sed-i <82407168+sed-i@users.noreply.github.com>
2023-09-22 08:51:35 -0400

    Bump LIBPATCH

--- lib/charms/tls_certificates_interface/v2/tls_certificates.py
+++ lib/charms/tls_certificates_interface/v2/tls_certificates.py
@@ -308,7 +308,7 @@ LIBAPI = 2
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 15
+LIBPATCH = 16
 
 PYDEPS = ["cryptography", "jsonschema"]
 
