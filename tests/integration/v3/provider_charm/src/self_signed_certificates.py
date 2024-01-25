b4252765940816a94e357950208a00e27b6ecae5
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-01-25 09:37:07 -0500

    Adds integration tests

--- tests/integration/v3/provider_charm/src/self_signed_certificates.py
+++ tests/integration/v3/provider_charm/src/self_signed_certificates.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+"""Methods used to generate self-signed certificates."""
+
 import datetime
+import re
 from typing import List, Optional
 
 from cryptography import x509
@@ -38,115 +42,71 @@ def generate_private_key(
     return key_bytes
 
 
-def generate_csr(
-    private_key: bytes,
-    common_name: str,
-    private_key_password: Optional[bytes] = None,
-    sans: Optional[List[str]] = None,
-    country: str = "US",
-) -> bytes:
-    """Generates a CSR using private key and subject.
-
-    Args:
-        private_key (bytes): Private key
-        private_key_password (bytes): Private key password
-        common_name (str): CSR common name.
-        sans (list): List of subject alternative names
-        country (str): CSR Issuing country
-
-    Returns:
-        bytes: CSR
-    """
-    signing_key = serialization.load_pem_private_key(private_key, password=private_key_password)
-    csr = x509.CertificateSigningRequestBuilder(
-        subject_name=x509.Name(
-            [
-                x509.NameAttribute(x509.NameOID.COUNTRY_NAME, country),
-                x509.NameAttribute(x509.NameOID.COMMON_NAME, common_name),
-            ]
-        )
-    )
-    if sans:
-        csr.add_extension(
-            x509.SubjectAlternativeName([x509.DNSName(san) for san in sans]), critical=False
-        )
-    signed_certificate = csr.sign(signing_key, hashes.SHA256())  # type: ignore[arg-type]
-    return signed_certificate.public_bytes(serialization.Encoding.PEM)
-
-
 def generate_certificate(
     csr: bytes,
     ca: bytes,
     ca_key: bytes,
     ca_key_password: Optional[bytes] = None,
-    validity: int = 24 * 365,
+    validity: int = 365,
+    alt_names: list = None,
 ) -> bytes:
-    """Generates a TLS certificate based on a CSR.
+    """Generates a certificate based on CSR.
 
     Args:
-        csr (bytes): CSR
-        ca (bytes): CA Certificate
-        ca_key (bytes): CA private key
-        ca_key_password (bytes): CA Private key password
-        validity (int): Certificate validity (in hours)
+        csr: CSR Bytes
+        ca: Set the CA certificate, must be PEM format
+        ca_key: The CA key, must be PEM format; if not in CAfile
+        ca_key_password: The CA key password
+        validity: Validity
+        alt_names: Alternative names (optional)
 
     Returns:
         bytes: Certificate
     """
     csr_object = x509.load_pem_x509_csr(csr)
-    ca_object = x509.load_pem_x509_certificate(ca)
-    ca_subject = ca_object.subject
-    csr_subject = csr_object.subject
-    ca_country = ca_subject.get_attributes_for_oid(x509.NameOID.COUNTRY_NAME)[0].value
-    csr_common_name = csr_subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)[0].value
+    subject = csr_object.subject
     issuer = x509.load_pem_x509_certificate(ca).issuer
     private_key = serialization.load_pem_private_key(ca_key, password=ca_key_password)
-    subject = x509.Name(
-        [
-            x509.NameAttribute(x509.NameOID.COUNTRY_NAME, ca_country),
-            x509.NameAttribute(x509.NameOID.COMMON_NAME, csr_common_name),
-        ]
-    )
 
-    if validity > 0:
-        not_valid_before = datetime.datetime.utcnow()
-        not_valid_after = datetime.datetime.utcnow() + datetime.timedelta(hours=validity)
-    else:
-        not_valid_before = datetime.datetime.utcnow() + datetime.timedelta(hours=validity)
-        not_valid_after = datetime.datetime.utcnow() - datetime.timedelta(seconds=1)
     certificate_builder = (
         x509.CertificateBuilder()
         .subject_name(subject)
         .issuer_name(issuer)
         .public_key(csr_object.public_key())
         .serial_number(x509.random_serial_number())
-        .not_valid_before(not_valid_before)
-        .not_valid_after(not_valid_after)
+        .not_valid_before(datetime.datetime.utcnow())
+        .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=validity))
     )
 
-    certificate_builder._version = x509.Version.v3
+    if alt_names:
+        names = [x509.DNSName(n) for n in alt_names]
+        certificate_builder = certificate_builder.add_extension(
+            x509.SubjectAlternativeName(names),
+            critical=False,
+        )
+    certificate_builder._version = x509.Version.v1
     cert = certificate_builder.sign(private_key, hashes.SHA256())  # type: ignore[arg-type]
     return cert.public_bytes(serialization.Encoding.PEM)
 
 
 def generate_ca(
     private_key: bytes,
-    common_name: str,
+    subject: str,
     private_key_password: Optional[bytes] = None,
     validity: int = 365,
     country: str = "US",
 ) -> bytes:
-    """Generates a CA Certificate.
+    """Generates a CA certificate.
 
     Args:
-        private_key (bytes): Private key
+        private_key (bytes): Private key to use
+        subject (str): Certificate subject
         private_key_password (bytes): Private key password
-        common_name (str): Certificate common name.
-        validity (int): Certificate validity time (in days)
-        country (str): Certificate Issuing country
+        validity (int): Certificate validity (in days)
+        country (str): Country name
 
     Returns:
-        bytes: CA Certificate
+        bytes: Certificate CA (encoded in base64)
     """
     private_key_object = serialization.load_pem_private_key(
         private_key, password=private_key_password
@@ -154,14 +114,13 @@ def generate_ca(
     subject = issuer = x509.Name(
         [
             x509.NameAttribute(x509.NameOID.COUNTRY_NAME, country),
-            x509.NameAttribute(x509.NameOID.COMMON_NAME, common_name),
+            x509.NameAttribute(x509.NameOID.COMMON_NAME, subject),
         ]
     )
     subject_identifier_object = x509.SubjectKeyIdentifier.from_public_key(
         private_key_object.public_key()  # type: ignore[arg-type]
     )
     subject_identifier = key_identifier = subject_identifier_object.public_bytes()
-
     cert = (
         x509.CertificateBuilder()
         .subject_name(subject)
@@ -186,3 +145,44 @@ def generate_ca(
         .sign(private_key_object, hashes.SHA256())  # type: ignore[arg-type]
     )
     return cert.public_bytes(serialization.Encoding.PEM)
+
+
+def certificate_is_valid(certificate: bytes) -> bool:
+    """Returns whether a certificate is valid.
+
+    Args:
+        certificate:
+
+    Returns:
+        bool: True/False
+    """
+    try:
+        x509.load_pem_x509_certificate(certificate)
+        return True
+    except ValueError:
+        return False
+
+
+def parse_ca_chain(ca_chain_pem: str) -> List[str]:
+    """Returns list of certificates based on a PEM CA Chain file.
+
+    Args:
+        ca_chain_pem (str): String containing list of certificates. This string should look like:
+            -----BEGIN CERTIFICATE-----
+            <cert 1>
+            -----END CERTIFICATE-----
+            -----BEGIN CERTIFICATE-----
+            <cert 2>
+            -----END CERTIFICATE-----
+
+    Returns:
+        list: List of certificates
+    """
+    chain_list = re.findall(
+        pattern="(?=-----BEGIN CERTIFICATE-----)(.*?)(?<=-----END CERTIFICATE-----)",
+        string=ca_chain_pem,
+        flags=re.DOTALL,
+    )
+    if not chain_list:
+        raise ValueError("No certificate found in chain file")
+    return chain_list
