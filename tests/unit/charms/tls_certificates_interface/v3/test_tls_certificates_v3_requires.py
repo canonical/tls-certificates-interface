8fd4e3ee4b478787a704c5cc8201a3ad4f1812b8
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-02-21 18:34:24 -0500

    fix: return empty list when get_assigned_certificates given no tls relation

--- tests/unit/charms/tls_certificates_interface/v3/test_tls_certificates_v3_requires.py
+++ tests/unit/charms/tls_certificates_interface/v3/test_tls_certificates_v3_requires.py
@@ -971,6 +971,9 @@ class TestTLSCertificatesRequiresV2(unittest.TestCase):
         )
 
         assert len(self.harness.charm.certificates.get_assigned_certificates()) == 0
+    
+    def test_given_tls_relation_not_created_when_get_assigned_certificates_then_empty_list_returned(self):
+        assert self.harness.charm.certificates.get_assigned_certificates() == []
 
     def test_given_csrs_created_when_get_certificate_signing_requests_then_all_csrs_returned(self):
         relation_id = self.create_certificates_relation()
