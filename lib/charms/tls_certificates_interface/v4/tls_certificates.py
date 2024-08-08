b76cdf0fbbfb695901ed395414123f5f5b721212
guillaume <guillaume.belanger27@gmail.com>
2024-08-08 12:39:06 -0400

    chore: bump libpatch revision
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- lib/charms/tls_certificates_interface/v4/tls_certificates.py
+++ lib/charms/tls_certificates_interface/v4/tls_certificates.py
@@ -184,7 +184,7 @@ LIBAPI = 4
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 1
+LIBPATCH = 2
 
 PYDEPS = ["cryptography", "pydantic"]
 
