1bc586081a629c95a876d31eab110e8bfffbbe71
guillaume <guillaume.belanger27@gmail.com>
2024-08-15 19:31:49 -0400

    feat: add certificate helpers
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- tests/unit/charms/tls_certificates_interface/v4/test_tls_certificates_v4.py
+++ tests/unit/charms/tls_certificates_interface/v4/test_tls_certificates_v4.py
@@ -7,9 +7,10 @@ from datetime import datetime, timedelta, timezone
 from ipaddress import IPv6Address
 
 from charms.tls_certificates_interface.v4.tls_certificates import (
-    CertificateRequest,
     PrivateKey,
-    _generate_private_key,
+    generate_ca,
+    generate_csr,
+    generate_private_key,
 )
 from cryptography import x509
 from cryptography.hazmat.primitives import serialization
@@ -35,11 +36,14 @@ def validate_induced_data_from_pfx_is_equal_to_initial_data(
         induced_certificate_object,
         _,
     ) = pkcs12.load_key_and_certificates(pfx_file, password.encode())
+    assert induced_private_key_object
+    assert induced_certificate_object
     initial_private_key_object = load_pem_private_key(
         initial_private_key,
         password=None,
     )
-    induced_private_key = induced_private_key_object.private_bytes(  # type: ignore[union-attr]
+    assert initial_private_key_object
+    induced_private_key = induced_private_key_object.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.TraditionalOpenSSL,
         encryption_algorithm=serialization.NoEncryption(),
@@ -49,12 +53,12 @@ def validate_induced_data_from_pfx_is_equal_to_initial_data(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.PKCS1,
     )
-    induced_public_key_object = induced_private_key_object.public_key()  # type: ignore[union-attr]
+    induced_public_key_object = induced_private_key_object.public_key()
     induced_public_key = induced_public_key_object.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.PKCS1,
     )
-    induced_certificate = induced_certificate_object.public_bytes(  # type: ignore[union-attr]
+    induced_certificate = induced_certificate_object.public_bytes(
         encoding=serialization.Encoding.PEM
     )
 
@@ -64,7 +68,7 @@ def validate_induced_data_from_pfx_is_equal_to_initial_data(
 
 
 def test_given_no_password_when_generate_private_key_then_key_is_generated_and_loadable():
-    private_key = _generate_private_key()
+    private_key = generate_private_key()
 
     load_pem_private_key(data=str(private_key).encode(), password=None)
 
@@ -72,13 +76,16 @@ def test_given_no_password_when_generate_private_key_then_key_is_generated_and_l
 def test_given_key_size_provided_when_generate_private_key_then_private_key_is_generated():
     key_size = 1234
 
-    private_key = _generate_private_key(key_size=key_size)
+    private_key = generate_private_key(key_size=key_size)
 
     private_key_object = serialization.load_pem_private_key(
         str(private_key).encode(), password=None
     )
     assert isinstance(private_key_object, rsa.RSAPrivateKeyWithSerialization)
     assert private_key_object.key_size == key_size
+
+
+# calculate expiry notification time
 
 
 def test_given_provider_recommended_notification_time_when_calculate_expiry_notification_time_then_returns_provider_recommendation():  # noqa: E501
@@ -113,7 +120,7 @@ def test_given_negative_provider_recommended_notification_time_when_calculate_ex
     assert notification_time == expected_notification_time
 
 
-def test_given_requirer_and_provider_recommendations_are_invalid_whencalculate_expiry_notification_time_then_returns_calculated_notification_time():  # noqa: E501
+def test_given_requirer_and_provider_recommendations_are_invalid_when_calculate_expiry_notification_time_then_returns_calculated_notification_time():  # noqa: E501
     expiry_time = datetime(2024, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
     validity_start_time_in_hours = 240
     validity_start_time = expiry_time - timedelta(hours=validity_start_time_in_hours)
@@ -128,7 +135,7 @@ def test_given_requirer_and_provider_recommendations_are_invalid_whencalculate_e
     assert notification_time == expected_notification_time
 
 
-def test_given_negative_requirer_and_provider_recommendations_are_invalid_whencalculate_expiry_notification_time_then_returns_calculated_notification_time():  # noqa: E501
+def test_given_negative_requirer_and_provider_recommendations_are_invalid_when_calculate_expiry_notification_time_then_returns_calculated_notification_time():  # noqa: E501
     expiry_time = datetime(2024, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
     validity_start_time_in_hours = 240
     validity_start_time = expiry_time - timedelta(hours=validity_start_time_in_hours)
@@ -158,80 +165,109 @@ def test_given_validity_time_is_too_short_when_calculate_expiry_notification_tim
     assert notification_time == expected_notification_time
 
 
-class TestCertificateRequest:
-    def test_given_subject_and_private_key_when_generate_csr_then_csr_is_generated_with_provided_subject(  # noqa: E501
-        self,
-    ):
-        subject = "whatever"
-        private_key = PrivateKey(raw=generate_private_key_helper())
+# Generate CSR
 
-        certificate_request = CertificateRequest(common_name=subject)
 
-        csr = certificate_request.generate_csr(private_key=private_key)
+def test_given_subject_and_private_key_when_generate_csr_then_csr_is_generated_with_provided_subject():  # noqa: E501
+    common_name = "whatever"
+    private_key = PrivateKey(raw=generate_private_key_helper())
 
-        csr_object = x509.load_pem_x509_csr(data=str(csr).encode())
-        subject_list = list(csr_object.subject)
-        assert len(subject_list) == 2
-        assert subject == subject_list[0].value
-        uuid.UUID(str(subject_list[1].value))
+    csr = generate_csr(private_key=private_key, common_name=common_name)
 
-    def test_given_unique_id_set_to_false_when_generate_csr_then_csr_is_generated_without_unique_id(  # noqa: E501
-        self,
-    ):
-        private_key = PrivateKey(raw=generate_private_key_helper())
-        subject = "whatever subject"
-        certificate_request = CertificateRequest(common_name=subject)
+    csr_object = x509.load_pem_x509_csr(data=str(csr).encode())
+    subject_list = list(csr_object.subject)
+    assert len(subject_list) == 2
+    assert common_name == subject_list[0].value
+    uuid.UUID(str(subject_list[1].value))
 
-        csr = certificate_request.generate_csr(
-            private_key=private_key, add_unique_id_to_subject_name=False
-        )
 
-        csr_object = x509.load_pem_x509_csr(data=str(csr).encode())
-        subject_list = list(csr_object.subject)
-        assert subject == subject_list[0].value
+def test_given_unique_id_set_to_false_when_generate_csr_then_csr_is_generated_without_unique_id(  # noqa: E501
+):
+    private_key = PrivateKey(raw=generate_private_key_helper())
+    common_name = "whatever subject"
 
-    def test_given_localization_is_specified_when__generate_csr_then_csr_contains_localization(
-        self,
-    ):
-        private_key = _generate_private_key()
+    csr = generate_csr(
+        private_key=private_key, common_name=common_name, add_unique_id_to_subject_name=False
+    )
 
-        certificate_request = CertificateRequest(
-            common_name="my.demo.server",
-            sans_dns=frozenset(["my.demo.server"]),
-            country_name="CA",
-            state_or_province_name="Quebec",
-            locality_name="Montreal",
-        )
+    csr_object = x509.load_pem_x509_csr(data=str(csr).encode())
+    subject_list = list(csr_object.subject)
+    assert common_name == subject_list[0].value
 
-        csr = certificate_request.generate_csr(private_key=private_key)
 
-        csr_object = x509.load_pem_x509_csr(str(csr).encode())
-        assert (
-            csr_object.subject.get_attributes_for_oid(x509.NameOID.COUNTRY_NAME)[0].value == "CA"
-        )
-        assert (
-            csr_object.subject.get_attributes_for_oid(x509.NameOID.STATE_OR_PROVINCE_NAME)[0].value
-            == "Quebec"
-        )
-        assert (
-            csr_object.subject.get_attributes_for_oid(x509.NameOID.LOCALITY_NAME)[0].value
-            == "Montreal"
-        )
+def test_given_localization_is_specified_when_generate_csr_then_csr_contains_localization():
+    private_key = generate_private_key()
 
-    def test_given_ipv6_sans_when__generate_csr_then_csr_contains_ipv6_sans(self):
-        private_key = _generate_private_key()
+    csr = generate_csr(
+        private_key=private_key,
+        common_name="my.demo.server",
+        sans_dns=frozenset(["my.demo.server"]),
+        country_name="CA",
+        state_or_province_name="Quebec",
+        locality_name="Montreal",
+    )
 
-        certificate_request = CertificateRequest(
-            common_name="my.demo.server",
-            sans_dns=frozenset(["my.demo.server"]),
-            sans_ip=frozenset(["2001:db8::1", "2001:db8::2"]),
-        )
+    csr_object = x509.load_pem_x509_csr(str(csr).encode())
+    assert csr_object.subject.get_attributes_for_oid(x509.NameOID.COUNTRY_NAME)[0].value == "CA"
+    assert (
+        csr_object.subject.get_attributes_for_oid(x509.NameOID.STATE_OR_PROVINCE_NAME)[0].value
+        == "Quebec"
+    )
+    assert (
+        csr_object.subject.get_attributes_for_oid(x509.NameOID.LOCALITY_NAME)[0].value
+        == "Montreal"
+    )
 
-        csr = certificate_request.generate_csr(private_key=private_key)
 
-        csr_object = x509.load_pem_x509_csr(str(csr).encode())
-        sans = csr_object.extensions.get_extension_for_class(x509.SubjectAlternativeName).value
-        sans_ip = sans.get_values_for_type(x509.IPAddress)
-        assert len(sans_ip) == 2
-        assert IPv6Address("2001:db8::1") in sans_ip
-        assert IPv6Address("2001:db8::2") in sans_ip
+def test_given_ipv6_sans_when_generate_csr_then_csr_contains_ipv6_sans():
+    private_key = generate_private_key()
+
+    csr = generate_csr(
+        private_key=private_key,
+        common_name="my.demo.server",
+        sans_dns=frozenset(["my.demo.server"]),
+        sans_ip=frozenset(["2001:db8::1", "2001:db8::2"]),
+    )
+
+    csr_object = x509.load_pem_x509_csr(str(csr).encode())
+    sans = csr_object.extensions.get_extension_for_class(x509.SubjectAlternativeName).value
+    sans_ip = sans.get_values_for_type(x509.IPAddress)
+    assert len(sans_ip) == 2
+    assert IPv6Address("2001:db8::1") in sans_ip
+    assert IPv6Address("2001:db8::2") in sans_ip
+
+
+# Generate CA
+
+
+def test_given_ca_certificate_attributes_when_generate_ca_then_ca_is_generated_correctly():
+    common_name = "certifier.example.com"
+    private_key = PrivateKey(raw=generate_private_key_helper())
+
+    ca_certificate = generate_ca(
+        private_key=private_key,
+        validity=365,
+        common_name=common_name,
+        sans_dns=frozenset(["certifier.example.com"]),
+        sans_ip=frozenset(["1.2.3.4"]),
+        email_address="banana@gmail.com",
+        organization="Example",
+        organizational_unit="Example Unit",
+        country_name="CA",
+        state_or_province_name="Quebec",
+        locality_name="Montreal",
+    )
+
+    assert ca_certificate.common_name == common_name
+    expected_expiry = datetime.now(timezone.utc) + timedelta(days=365)
+    assert ca_certificate.expiry_time
+    assert abs(ca_certificate.expiry_time - expected_expiry) <= timedelta(seconds=1)
+    assert ca_certificate.email_address == "banana@gmail.com"
+    assert ca_certificate.organization == "Example"
+    assert ca_certificate.organizational_unit == "Example Unit"
+    assert ca_certificate.country_name == "CA"
+    assert ca_certificate.state_or_province_name == "Quebec"
+    assert ca_certificate.locality_name == "Montreal"
+    assert ca_certificate.sans_dns == frozenset(["certifier.example.com"])
+    assert ca_certificate.sans_ip == frozenset(["1.2.3.4"])
+    assert ca_certificate.sans_oid == frozenset()
