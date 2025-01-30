0cece47eeef521110c8049082910c025cd2ee9ba
yazansalti <yazan.salti@canonical.com>
2025-01-30 18:15:47 +0100

    Bumps libpatch

--- lib/charms/tls_certificates_interface/v4/tls_certificates.py
+++ lib/charms/tls_certificates_interface/v4/tls_certificates.py
@@ -52,7 +52,7 @@ LIBAPI = 4
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 7
+LIBPATCH = 8
 
 PYDEPS = ["cryptography", "pydantic"]
 
